21be226d2c320b091fbbdf5a55297c231dfdd679
Mathius Johnson <devachievem@gmail.com>
2021-01-11 21:05:56 -0800

    updated data

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -36,7 +36,7 @@ class Contact extends Component {
          <div className="row">
             <div className="eight columns">
 
-               <form action="" method="post" id="contactForm" name="contactForm">
+               <form action="form-to-email.php" method="post" id="contactForm" name="contactForm">
 					<fieldset>
 
                   <div>
